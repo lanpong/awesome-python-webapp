d95cd10d0398a19441dfa48e28557878615e2c02
lanpong <hdy95@outlook.com>
2018-10-04 15:44:35 +0800

    Update orm.py
    update

--- www/orm.py
+++ www/orm.py
@@ -134,7 +134,7 @@ class ModelMetaclass(type):
         attrs['__mappings__'] = mappings
         attrs['__table__'] = tableName
         attrs['__primary_key__'] = primaryKey
-        attrs['__fields__'] = fields # 除主键外的属性名
+        attrs['__fields__'] = fields
         attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
         attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
         attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
